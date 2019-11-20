019f510ac13d17d5223159b7c48eb047b058bfe1
Lluis Suros <lluissuros@gmail.com>
2019-11-21 00:56:09 +0100

    add some Links to test routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import Home from "./pages/Home";
 import Contacts from "./pages/Contacts";
@@ -10,7 +10,17 @@ function App(props) {
     <AuthContext.Provider value={false}>
       <Router>
         <div>
-          <ul>...</ul>
+          <ul>
+            <li>
+              <Link to="/">Home Page</Link>
+            </li>
+            <li>
+              <Link to="/contacts">Contacts Page</Link>
+            </li>
+            <li>
+              <Link to="/testOther">testOther Page</Link>
+            </li>
+          </ul>
           <Route exact path="/" component={Home} />
           <PrivateRoute path="/contacts" component={Contacts} />
         </div>
